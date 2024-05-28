9239446d75ab397dd7b92bcf32e77b25f11b5ff3
Nergui Badarch <nerguibadarch@gmail.com>
2024-05-28 11:40:58 +0800

    default model

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./root/serviceAccountKey.json');
+const serviceAccount = require('/root/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
